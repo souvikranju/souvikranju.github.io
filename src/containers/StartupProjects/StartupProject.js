9883dabca8f9740b681448f8ca84b57e7e5b78b4
Souvik Pal <souvik.ranju@gmail.com>
2026-05-12 14:19:36 +0530

    FIX: Experience, project not visible on small width

--- src/containers/StartupProjects/StartupProject.js
+++ src/containers/StartupProjects/StartupProject.js
@@ -18,7 +18,7 @@ export default function StartupProject() {
     return null;
   }
   return (
-    <Fade bottom duration={1000} distance="20px">
+    <Fade bottom duration={1000} distance="20px" fraction={0}>
       <div className="main" id="projects">
         <div>
           <h1 className="skills-heading">{bigProjects.title}</h1>
